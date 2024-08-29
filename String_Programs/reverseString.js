d9f2e714802df07c1568d7e0514391fae8479bed
Pavan Kumar Kuruva <kpavankumark9783@gmail.com>
2024-08-29 15:51:50 +0530

    Reverse String Programm

---
+++ String_Programs/reverseString.js
@@ -0,0 +1,16 @@
+function reverseString(str) {
+    let reversed = '';
+    for(let i=str.length-1; i>=0; i--) {
+        reversed += str[i]
+        // console.log(reversed );        
+    }
+    return reversed;
+}
+
+console.log(reverseString("hello"));
+
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+console.log(reverseString("hello")); // Output: "olleh"
