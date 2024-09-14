bf14c7c261f24c834edfcec59f3383ff3f23f481
Pavan Kumar Kuruva <kpavankumark9783@gmail.com>
2024-09-14 15:02:18 +0530

    Anagram Program another method(in object) in JS

--- String_Programs/CheckAnagram.js
+++ String_Programs/CheckAnagram.js
@@ -10,6 +10,7 @@ let str2 = "listen";
 // let str1 = "cat";
 // let str2 = "dog"
 
+// 1.
 function checkAnagram(str1, str2) {
     let word1 = str1.toLowerCase();
     let word2 = str2.toLowerCase();
@@ -23,3 +24,38 @@ function checkAnagram(str1, str2) {
 }
 
 console.log(checkAnagram(str1, str2));
+
+
+
+// 2.
+function CheckAnagram(str1, str2) {
+  //   To remove space and make lowercase
+  str1 = str1.trim().toLowerCase();
+  str2 = str2.trim().toLowerCase();
+
+  if (str1.length !== str2.length) return false;
+
+  let countStr1 = findOccurrence(str1);
+  let countStr2 = findOccurrence(str2);
+
+  function findOccurrence(str) {
+    let obj = {};
+    for (let i = 0; i < str.length; i++) {
+      if (obj[str[i]]) {
+        obj[str[i]] = obj[str[i]] + 1;
+      } else {
+        obj[str[i]] = 1;
+      }
+    }
+    return obj;
+  }
+
+  for (let key in countStr1) {
+    if (countStr1[key] !== countStr2[key]) return false;
+  }
+  return true;
+}
+
+console.log(CheckAnagram(str1, str2));
+
+
